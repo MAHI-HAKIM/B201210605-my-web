2b30fa275c0e4f8a813d34dd9e1022c2d1770c80
MAHI-HAKIM <103943510+MAHI-HAKIM@users.noreply.github.com>
2022-05-09 19:26:05 +0300

    added about section and a login button on the home section
    several changes have been made to the home page and cool animation have been added on the text and other features

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 $(document).ready(function () {
-  $(".js--scroll-to-aboutme").click(function () {});
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 100) {
+      $(".navbar").addClass("apear");
+    } else {
+      $(".navbar").removeClass("apear");
+    }
+  });
 
-  const toggleButton = document.getElementsByClassName("nav-toggle")[0];
-  const navbarLinks = document.getElementsByClassName("navibar-links")[0];
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
 
-  toggleButton.addEventListener("click", () => {
-    navbarLinks.classList.toggle("active");
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: [
+      "Ethiopia",
+      "Land of Origin",
+      "Owner of Nature",
+      "Land of Diversity",
+    ],
+    typeSpeed: 90,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  var typed = new Typed(".typing-2", {
+    strings: ["Code", "Photography", "Gaming", "Designing", "Football"],
+    typeSpeed: 90,
+    backSpeed: 60,
+    loop: true,
   });
 });
